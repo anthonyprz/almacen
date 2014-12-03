08a14fdac1b86a9942d888b853b4cad879e35af5
Sonia Ortiz de Arri <soniaodea@gmail.com>
2014-12-03 13:25:49 +0100

    miercoles

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
@@ -11,79 +10,81 @@ public class Main
 	
 	public static void main(String[] args) 
 	{
-		
-
-			Scanner sc = new Scanner (System.in);
-			//variables 
-			String nombre;
-			String cif;
-			//Direccion direccion;
-			Contacto personaContacto;
-			int numeroDistribuidores;
-			String direccion;
-			
-
-		System.out.println("\n¿cuantos distribuidores quieres guardar?");
-		numeroDistribuidores = sc.nextInt();
-
 		ArrayList<Distribuidor> distribuidores = new ArrayList<Distribuidor>();
+		FileInputStream fichero = new FileInputStream("./distribuidores.txt");
+		InputStreamReader isr = new InputStreamReader(fichero);
+		BufferedReader br = new BufferedReader(isr);
+		
+		String linea = br.readLine();
+		String [] sitio = null;
 			
 
-		for (int c=0; c<numeroDistribuidores ;c++ )
-		{
-			System.out.println("\n<><><><>distribuidores<><><><>");
-			
-			Distribuidor distribuidor = new Distribuidor();
-			//Contacto cont = new Contacto();
-
-
-			System.out.println("\nintroduce el nombre del distribuidor");
-			nombre = sc.next();
-			distribuidor.setNombre(nombre);
-			System.out.println("\nintroduce el CIF del distribuidor");
-			cif = sc.next();
-			distribuidor.setCif(cif);
-			
-					//direccion 
+			while (linea != null) {
 				
-					
+				Distribuidor distribuidor = new Distribuidor();
+				Direccion direccion = new Direccion();
+				Contacto contacto = new Contacto();
 				
-					//do {System.out.println("\nahora vamos con la dirección");
-						System.out.println("\nvas a introducir una direccion");	
-									
-							
-								Direccion direc = new Direccion();
-								String ciudad;
-								int codigoPostal;
-								String calle;
-								int numero;
-								//direccion = sc.next();
-								//distribuidor.setDireccion(direccion);	
-								System.out.println("\nintroduce la ciudad del distribuidor");
-								ciudad = sc.next();							
-								direc.setCiudad(ciudad);
-								System.out.println("\nintroduce el codigo postal");
-								codigoPostal = sc.nextInt();
-								direc.setCodigoPostal(codigoPostal);
-								System.out.println("\nintroduce la calle");
-								calle = sc.next();
-								direc.setCalle(calle);
-								System.out.println("\nintroduce el numero");
-								numero = sc.nextInt();
-								direc.setNumero(numero);
 
-						}
-						
-						
-					
-					
-					//fin de direccion
+				sitio = linea.split("#");
+				String distri = sitio[0];
 
-			//System.out.println("\nintroduce el numero de contacto del distribuidor");
-			//personaContacto = sc.nextInt();
-			//distribuidor.setPersonaContacto(personaContacto);
-			
-		//}while (direccion = 1);
+				String [] sitio2 = distri.split(",");
+
+				distri.setNombre(sitio2[0]);
+				distri.setCif(sitio2[1]);
+
+				distri = sitio[1];
+				sitio2 = distri.split(",");
+
+				direccion.setDireccion(sitio2[0]);
+				int entero = Integer.parseInt(sitio2[1]);
+				direccion.setCodigoPostal(entero);
+				direccion.setCalle(sitio2[2]);
+				entero = Integer.parseInt(sitio2[4]);
+				direccion.setNumero(entero);
+
+				distribuidor.setDireccion(direccion);
+
+				distri = sitio[2];
+				sitio2 = distri.split(",");
+
+				contacto.setNombre(sitio2[0]);
+				contacto.setApellido(sitio2[1]);
+				entero = Integer.parseInt(sitio2[2]);
+				contacto.setNumTelefonico(entero);
+				entero = Integer.parseInt(sitio2[3]);
+				contacto.setEdad(entero);
+
+				distribuidor.setContacto(contacto);
+				distribuidores.add(distribuidor);
+				linea = br.readLine();
+
+		}	
+
+		for (int a = 0; a < distribuidores ; a++ ) {
+
+			Direccion direcc = new Direccion();
+			Contacto contact = new Contacto();
+
+			System.out.println((i + 1) + "º proveedor");
+			System.out.println("<><><><><><><><><><><><><><><><><><><><>");
+			System.out.println("Nombre: " + (distribuidores.get(a).getNombre()));
+			System.out.println("Cif: " + (distribuidores.get(a).getCif()));
+			direcc = distribuidores.get(a).getDireccion();
+			System.out.println("\tCiudad: " + direc.getCiudad());
+			System.out.println("\tCod.postal: " + direc.getCodigoPostal());
+			System.out.println("\tCalle: " + direc.getCalle());
+			System.out.println("\tCalle: " + direc.getNumero());
+			direcc = distribuidores.get(a).getContacto();
+			System.out.println("\tNombre: " + contact.getNombre());
+			System.out.println("\tApellido: " + contact.getApellido());
+			System.out.println("\tApellido: " + contact.getNumTelefonico());
+			System.out.println("\tApellido: " + contact.getEdad());
+
+
+
+		}
 
 	}
 }
