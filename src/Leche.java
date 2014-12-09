ca24242cc0416d9ab0903f5496e645f4490cd18a
anthony <thony_prz@outlook.com>
2014-12-09 09:26:39 +0100

    Update Leche.java

--- src/Leche.java
+++ src/Leche.java
@@ -1,37 +1,73 @@
 public class  Leche {
+
 	
+
 	//propiedades 
+
 	private String tipo;
+
 	private String procedencia;
+
 	private Double eurosLitro;
+
 	private Distribuidor distribuidor;
 
 
+
+
+
 		//-------------getters-------------
+
 		public String getTipo(){
+
 			return this.tipo;
+
 		}
+
 		public String getProcedencia(){
+
 			return this.procedencia;
+
 		}
+
 		public Double getEurosLitro(){
+
 			return this.eurosLitro;
+
 		}
+
 		public Distribuidor getDistribuidor(){
+
 			return this.distribuidor;
+
 		}
+
 		//-------------setters----------------
+
 		public void setTipo (String tipo){
+
 			this.tipo = tipo;
+
 		}
+
 		public void setProcedencia (String procedencia){
+
 			this.procedencia = procedencia;
+
 		}
-		public void Double setEurosLitro(Double eurosLitro){
+
+		public void  setEurosLitro(Double eurosLitro){
+
 			this.eurosLitro = eurosLitro;
+
 		}
-		public void Distribuidor setDistribuidor (Distribuidor distribuidor){
-			this distribuidor = distribuidor;
+
+		public void setDistribuidor (Distribuidor distribuidor){
+
+			this.distribuidor = distribuidor;
+
 		}
+
+
 
 }
